6ce59d7dadba257de1fe658df5ad6e110cae59b5
WhereIsLucas <lucas@retinens.be>
2023-09-03 13:42:53 +0200

    colors are ok

--- ShowGenerator/3GenerateBase.lua
+++ ShowGenerator/3GenerateBase.lua
@@ -25,53 +25,53 @@ function generateGroups()
 end
 
 function generateAppearances()
-    local White = {R= 255, G= 255, B= 255, Gel= 'MA.white'}
-    local CTO = {R= 255, G= 210, B= 159, Gel= "Cinegel.Roscosun 1/2 CTO"}
-    local CTB = {R= 173, G= 255, B= 246, Gel= "Cinegel.Half Blue CTB"}
-    local Red = {R= 255, G= 0, B= 0, Gel= "MA.red"}
-    local Orange = {R= 255, G= 127, B= 0, Gel= "MA.orange"}
-    local Yellow = {R= 255, G= 255, B= 0, Gel= "MA.yellow"}
-    local FernGreen = {R= 0, G= 255, B= 0, Gel= "MA.Fern Green"}
-    local Green = {R= 0, G= 127, B= 0, Gel= "MA.Green"}
-    local SeaGreen = {R= 0, G= 255, B= 127, Gel= "MA.Sea Green"}
-    local Cyan = {R= 0, G= 255, B= 255, Gel= "MA.Cyan"}
-    local Lavender = {R= 127, G= 0, B= 255, Gel= "MA.Lavender"}
-    local Blue = {R= 0, G= 0, B= 255, Gel= "MA.Blue"}
-    local Violet = {R= 127, G= 0, B= 255, Gel= "MA.Violet"}
-    local Magenta = {R= 255, G= 0, B= 255, Gel= "MA.Magenta"}
-    local Pink = {R= 255, G= 0, B= 127, Gel= "MA.Pink"}
+    local White = { id = 1, R = 255, G = 255, B = 255, GelCategory = 'MA', GelName = 'white' }
+    local CTO = { id = 2, R = 255, G = 210, B = 159, GelCategory = "Cinegel", GelName = "Roscosun 1/2 CTO" }
+    local CTB = { id = 3, R = 173, G = 255, B = 246, GelCategory = "Cinegel", GelName = "Half Blue CTB" }
+    local Red = { id = 4, R = 255, G = 0, B = 0, GelCategory = "MA", GelName = "red" }
+    local Orange = { id = 5, R = 255, G = 127, B = 0, GelCategory = "MA", GelName = "orange" }
+    local Yellow = { id = 6, R = 255, G = 255, B = 0, GelCategory = "MA", GelName = "yellow" }
+    local FernGreen = { id = 7, R = 0, G = 255, B = 0, GelCategory = "MA", GelName = "Fern Green" }
+    local Green = { id = 8, R = 0, G = 127, B = 0, GelCategory = "MA", GelName = "Green" }
+    local SeaGreen = { id = 9, R = 0, G = 255, B = 127, GelCategory = "MA", GelName = "Sea Green" }
+    local Cyan = { id = 10, R = 0, G = 255, B = 255, GelCategory = "MA", GelName = "Cyan" }
+    local Lavender = { id = 11, R = 127, G = 0, B = 255, GelCategory = "MA", GelName = "Lavender" }
+    local Blue = { id = 12, R = 0, G = 0, B = 255, GelCategory = "MA", GelName = "Blue" }
+    local Violet = { id = 13, R = 127, G = 0, B = 255, GelCategory = "MA", GelName = "Violet" }
+    local Magenta = { id = 14, R = 255, G = 0, B = 255, GelCategory = "MA", GelName = "Magenta" }
+    local Pink = { id = 15, R = 255, G = 0, B = 127, GelCategory = "MA", GelName = "Pink" }
 
-    generateColorAppearance('White', White, 1)
-    generateColorAppearance('CTO', CTO, 2)
-    generateColorAppearance('CTB', CTB, 3)
-    generateColorAppearance('Red', Red, 4)
-    generateColorAppearance('Orange', Orange, 5)
-    generateColorAppearance('Yellow', Yellow, 6)
-    generateColorAppearance('Fern Green', FernGreen, 7)
-    generateColorAppearance('Green', Green, 8)
-    generateColorAppearance('Sea Green', SeaGreen, 9)
-    generateColorAppearance('Cyan', Cyan, 10)
-    generateColorAppearance('Lavender', Lavender, 11)
-    generateColorAppearance('Blue', Blue, 12)
-    generateColorAppearance('Violet', Violet, 13)
-    generateColorAppearance('Magenta', Magenta, 14)
-    generateColorAppearance('Pink', Pink, 15)
+    generateColorAppearance('White', White)
+    generateColorAppearance('CTO', CTO)
+    generateColorAppearance('CTB', CTB)
+    generateColorAppearance('Red', Red)
+    generateColorAppearance('Orange', Orange)
+    generateColorAppearance('Yellow', Yellow)
+    generateColorAppearance('Fern Green', FernGreen)
+    generateColorAppearance('Green', Green)
+    generateColorAppearance('Sea Green', SeaGreen)
+    generateColorAppearance('Cyan', Cyan)
+    generateColorAppearance('Lavender', Lavender)
+    generateColorAppearance('Blue', Blue)
+    generateColorAppearance('Violet', Violet)
+    generateColorAppearance('Magenta', Magenta)
+    generateColorAppearance('Pink', Pink)
 end
 
-function generateColorAppearance(label, color, i)
-    Cmd('Store Appearance '..100+i..' /Overwrite')
-    Cmd('Label Appearance '..100+i..' "'..label..'"')
-    Cmd('Set Appearance '..100+i..' "BackR" '..color.R)
-    Cmd('Set Appearance '..100+i..' "BackG" '..color.G)
-    Cmd('Set Appearance '..100+i..' "BackB" '..color.B)
-    Cmd('Set Appearance '..100+i..' "BackAlpha" 255')
+function generateColorAppearance(label, data)
+    Cmd('Store Appearance '..100+data.id..' /Overwrite')
+    Cmd('Label Appearance '..100+data.id..' "'..label..'"')
+    Cmd('Set Appearance '..100+data.id..' "BackR" '.. data.R)
+    Cmd('Set Appearance '..100+data.id..' "BackG" '.. data.G)
+    Cmd('Set Appearance '..100+data.id..' "BackB" '.. data.B)
+    Cmd('Set Appearance '..100+data.id..' "BackAlpha" 255')
 
-    Cmd('Store Appearance '..200+i..' /Overwrite')
-    Cmd('Label Appearance '..200+i..' "'..label..' Active"')
-    Cmd('Set Appearance '..200+i..' "BackR" '..color.R)
-    Cmd('Set Appearance '..200+i..' "BackG" '..color.G)
-    Cmd('Set Appearance '..200+i..' "BackB" '..color.B)
-    Cmd('Set Appearance '..200+i..' "BackAlpha" 255')
+    Cmd('Store Appearance '..200+data.id..' /Overwrite')
+    Cmd('Label Appearance '..200+data.id..' "'..label..' Active"')
+    Cmd('Set Appearance '..200+data.id..' "BackR" '.. data.R)
+    Cmd('Set Appearance '..200+data.id..' "BackG" '.. data.G)
+    Cmd('Set Appearance '..200+data.id..' "BackB" '.. data.B)
+    Cmd('Set Appearance '..200+data.id..' "BackAlpha" 255')
 end
 
 
--- ShowGenerator/4GenerateShow.lua
+++ ShowGenerator/4GenerateShow.lua
@@ -6,45 +6,45 @@ function generateColorPresets()
     Cmd('ClearAll')
     Cmd('Group 1 thru')
 
-    local White = { R = 255, G = 255, B = 255, GelCategory = 'MA', GelName = 'white' }
-    local CTO = { R = 255, G = 210, B = 159, GelCategory = "Cinegel", GelName = "Roscosun 1/2 CTO" }
-    local CTB = { R = 173, G = 255, B = 246, GelCategory = "Cinegel", GelName = "Half Blue CTB" }
-    local Red = { R = 255, G = 0, B = 0, GelCategory = "MA", GelName = "red" }
-    local Orange = { R = 255, G = 127, B = 0, GelCategory = "MA", GelName = "orange" }
-    local Yellow = { R = 255, G = 255, B = 0, GelCategory = "MA", GelName = "yellow" }
-    local FernGreen = { R = 0, G = 255, B = 0, GelCategory = "MA", GelName = "Fern Green" }
-    local Green = { R = 0, G = 127, B = 0, GelCategory = "MA", GelName = "Green" }
-    local SeaGreen = { R = 0, G = 255, B = 127, GelCategory = "MA", GelName = "Sea Green" }
-    local Cyan = { R = 0, G = 255, B = 255, GelCategory = "MA", GelName = "Cyan" }
-    local Lavender = { R = 127, G = 0, B = 255, GelCategory = "MA", GelName = "Lavender" }
-    local Blue = { R = 0, G = 0, B = 255, GelCategory = "MA", GelName = "Blue" }
-    local Violet = { R = 127, G = 0, B = 255, GelCategory = "MA", GelName = "Violet" }
-    local Magenta = { R = 255, G = 0, B = 255, GelCategory = "MA", GelName = "Magenta" }
-    local Pink = { R = 255, G = 0, B = 127, GelCategory = "MA", GelName = "Pink" }
+    local White = { id = 1, R = 255, G = 255, B = 255, GelCategory = 'MA', GelName = 'white' }
+    local CTO = { id = 2, R = 255, G = 210, B = 159, GelCategory = "Cinegel", GelName = "Roscosun 1/2 CTO" }
+    local CTB = { id = 3, R = 173, G = 255, B = 246, GelCategory = "Cinegel", GelName = "Half Blue CTB" }
+    local Red = { id = 4, R = 255, G = 0, B = 0, GelCategory = "MA", GelName = "red" }
+    local Orange = { id = 5, R = 255, G = 127, B = 0, GelCategory = "MA", GelName = "orange" }
+    local Yellow = { id = 6, R = 255, G = 255, B = 0, GelCategory = "MA", GelName = "yellow" }
+    local FernGreen = { id = 7, R = 0, G = 255, B = 0, GelCategory = "MA", GelName = "Fern Green" }
+    local Green = { id = 8, R = 0, G = 127, B = 0, GelCategory = "MA", GelName = "Green" }
+    local SeaGreen = { id = 9, R = 0, G = 255, B = 127, GelCategory = "MA", GelName = "Sea Green" }
+    local Cyan = { id = 10, R = 0, G = 255, B = 255, GelCategory = "MA", GelName = "Cyan" }
+    local Lavender = { id = 11, R = 127, G = 0, B = 255, GelCategory = "MA", GelName = "Lavender" }
+    local Blue = { id = 12, R = 0, G = 0, B = 255, GelCategory = "MA", GelName = "Blue" }
+    local Violet = { id = 13, R = 127, G = 0, B = 255, GelCategory = "MA", GelName = "Violet" }
+    local Magenta = { id = 14, R = 255, G = 0, B = 255, GelCategory = "MA", GelName = "Magenta" }
+    local Pink = { id = 15, R = 255, G = 0, B = 127, GelCategory = "MA", GelName = "Pink" }
 
-    generateColorPreset('White', White, 1)
-    generateColorPreset('CTO', CTO, 2)
-    generateColorPreset('CTB', CTB, 3)
-    generateColorPreset('Red', Red, 4)
-    generateColorPreset('Orange', Orange, 5)
-    generateColorPreset('Yellow', Yellow, 6)
-    generateColorPreset('Fern Green', FernGreen, 7)
-    generateColorPreset('Green', Green, 8)
-    generateColorPreset('Sea Green', SeaGreen, 9)
-    generateColorPreset('Cyan', Cyan, 10)
-    generateColorPreset('Lavender', Lavender, 11)
-    generateColorPreset('Blue', Blue, 12)
-    generateColorPreset('Violet', Violet, 13)
-    generateColorPreset('Magenta', Magenta, 14)
-    generateColorPreset('Pink', Pink, 15)
+    generateColorPreset('White', White)
+    generateColorPreset('CTO', CTO)
+    generateColorPreset('CTB', CTB)
+    generateColorPreset('Red', Red)
+    generateColorPreset('Orange', Orange)
+    generateColorPreset('Yellow', Yellow)
+    generateColorPreset('Fern Green', FernGreen)
+    generateColorPreset('Green', Green)
+    generateColorPreset('Sea Green', SeaGreen)
+    generateColorPreset('Cyan', Cyan)
+    generateColorPreset('Lavender', Lavender)
+    generateColorPreset('Blue', Blue)
+    generateColorPreset('Violet', Violet)
+    generateColorPreset('Magenta', Magenta)
+    generateColorPreset('Pink', Pink)
 
 end
 
-function generateColorPreset(label, data, i)
-    Cmd('At Gel "' .. data.GelCategory .. '."' .. data.GelName .. '"')
-    Cmd('Store Preset 4.' .. i .. ' /Overwrite')
-    Cmd('Label Preset 4.' .. i .. ' "' .. label .. '"')
-    Cmd('Assign Appearance "' .. label .. '" At Preset 4.' .. i)
+function generateColorPreset(label, data)
+    Cmd('At Gel "' .. data.GelCategory .. '"."' .. data.GelName .. '"')
+    Cmd('Store Preset 4.' .. data.id .. ' /Overwrite')
+    Cmd('Label Preset 4.' .. data.id .. ' "' .. label .. '"')
+    Cmd('Assign Appearance "' .. label .. '" At Preset 4.' .. data.id)
 end
 
 function generateDimmerPresets()
@@ -72,25 +72,26 @@ end
 
 function generateColorSequences()
     Cmd('ClearAll')
-    local GlobalSeqNum = 3000
-    local SingleSeqNum = 4000
+
+    local SingleSeqNum = 1000
     for k = 1, 6, 1 do
         for i = 1, 15, 1 do
             local colorNum = i
-            Cmd('Group ' .. k)
-            GlobalSeqNum = GlobalSeqNum + 1
-            Cmd('At Preset 4.' .. colorNum)
-            Cmd('Store Sequence ' .. k .. '/C')
-            Cmd('Label Sequence ' .. k .. ' "Colors L"' .. k)
-            Cmd('Assign Sequence ' .. k .. ' At Page 1.30' .. k .. ' /O')
-
-            SingleSeqNum = SingleSeqNum + 1
             Cmd('ClearAll')
             Cmd('Group ' .. k)
             Cmd('At Preset 4.' .. colorNum)
+            Cmd('Store Sequence ' .. 100+k .. '/C')
+            Cmd('Label Sequence ' .. 100+k .. ' "Colors L"' .. k)
+            Cmd('Assign Sequence ' .. 100+k .. ' At Page 1.30' .. k .. ' /O')
+
+            Cmd('ClearAll')
+            Cmd('Group ' .. k)
+            Cmd('At Preset 4.' .. colorNum)
+            Cmd('Delete Sequence ' .. SingleSeqNum)
             Cmd('Store Sequence ' .. SingleSeqNum .. '/C')
-            Cmd('Label Sequence ' .. SingleSeqNum .. ' \"Color L\"'.. k .. colorNum)
+            Cmd('Label Sequence ' .. SingleSeqNum .. ' \"Color L\"'.. k .." ".. colorNum)
             Cmd('Assign Appearance ' .. 100 + colorNum .. ' At Sequence ' .. SingleSeqNum .. ' /O')
+            SingleSeqNum = SingleSeqNum + 1
         end
     end
 end
