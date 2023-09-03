1b8dabcb0cad5be21c025673181e9cda1d0a225f
WhereIsLucas <lucas@retinens.be>
2023-09-03 10:48:33 +0200

    wip

--- ShowGenerator/3GenerateBase.lua
+++ ShowGenerator/3GenerateBase.lua
@@ -44,7 +44,100 @@ function generateApparences()
     Cmd('Set Appearance 102 "BackR" 255')
     Cmd('Set Appearance 102 "BackG" 210')
     Cmd('Set Appearance 102 "BackB" 159')
+
+    Cmd('Store Appearance 103')
+    Cmd('Label Appearance 103 "CTB"')
+    Cmd('Set Appearance 103 "BackR" 159')
+    Cmd('Set Appearance 103 "BackG" 210')
+    Cmd('Set Appearance 103 "BackB" 255')
     Cmd('Set Appearance 102 "BackAlpha" 255')
+
+    Cmd('Store Appearance 104')
+    Cmd('Label Appearance 104 "Red"')
+    Cmd('Set Appearance 104 "BackR" 255')
+    Cmd('Set Appearance 104 "BackG" 0')
+    Cmd('Set Appearance 104 "BackB" 0')
+    Cmd('Set Appearance 102 "BackAlpha" 255')
+
+    Cmd('Store Appearance 105')
+    Cmd('Label Appearance 105 "Orange"')
+    Cmd('Set Appearance 105 "BackR" 255')
+    Cmd('Set Appearance 105 "BackG" 127')
+    Cmd('Set Appearance 105 "BackB" 0')
+    Cmd('Set Appearance 102 "BackAlpha" 255')
+
+    Cmd('Store Appearance 106')
+    Cmd('Label Appearance 106 "Yellow"')
+    Cmd('Set Appearance 106 "BackR" 255')
+    Cmd('Set Appearance 106 "BackG" 255')
+    Cmd('Set Appearance 106 "BackB" 0')
+    Cmd('Set Appearance 102 "BackAlpha" 255')
+
+    Cmd('Store Appearance 107')
+    Cmd('Label Appearance 107 "Fern Green"')
+    Cmd('Set Appearance 107 "BackR" 127')
+    Cmd('Set Appearance 107 "BackG" 255')
+    Cmd('Set Appearance 107 "BackB" 0')
+
+    Cmd('Store Appearance 108')
+    Cmd('Label Appearance 108 "Green"')
+    Cmd('Set Appearance 108 "BackR" 0')
+    Cmd('Set Appearance 108 "BackG" 255')
+    Cmd('Set Appearance 108 "BackB" 0')
+    Cmd('Set Appearance 102 "BackAlpha" 255')
+
+    Cmd('Store Appearance 109')
+    Cmd('Label Appearance 109 "Sea Green"')
+    Cmd('Set Appearance 109 "BackR" 0')
+    Cmd('Set Appearance 109 "BackG" 255')
+    Cmd('Set Appearance 109 "BackB" 127')
+    Cmd('Set Appearance 102 "BackAlpha" 255')
+
+    Cmd('Store Appearance 110')
+    Cmd('Label Appearance 110 "Cyan"')
+    Cmd('Set Appearance 110 "BackR" 0')
+    Cmd('Set Appearance 110 "BackG" 255')
+    Cmd('Set Appearance 110 "BackB" 255')
+    Cmd('Set Appearance 102 "BackAlpha" 255')
+
+    Cmd('Store Appearance 111')
+    Cmd('Label Appearance 111 "Lavender"')
+    Cmd('Set Appearance 111 "BackR" 127')
+    Cmd('Set Appearance 111 "BackG" 0')
+    Cmd('Set Appearance 111 "BackB" 255')
+    Cmd('Set Appearance 102 "BackAlpha" 255')
+
+    Cmd('Store Appearance 112')
+    Cmd('Label Appearance 112 "Blue"')
+    Cmd('Set Appearance 112 "BackR" 0')
+    Cmd('Set Appearance 112 "BackG" 0')
+    Cmd('Set Appearance 112 "BackB" 255')
+    Cmd('Set Appearance 102 "BackAlpha" 255')
+
+    Cmd('Store Appearance 113')
+    Cmd('Label Appearance 113 "Violet"')
+    Cmd('Set Appearance 113 "BackR" 127')
+    Cmd('Set Appearance 113 "BackG" 0')
+    Cmd('Set Appearance 113 "BackB" 255')
+    Cmd('Set Appearance 102 "BackAlpha" 255')
+
+    Cmd('Store Appearance 114')
+    Cmd('Label Appearance 114 "Magenta"')
+    Cmd('Set Appearance 114 "BackR" 255')
+    Cmd('Set Appearance 114 "BackG" 0')
+    Cmd('Set Appearance 114 "BackB" 255')
+    Cmd('Set Appearance 102 "BackAlpha" 255')
+
+    Cmd('Store Appearance 115')
+    Cmd('Label Appearance 115 "Pink"')
+    Cmd('Set Appearance 115 "BackR" 255')
+    Cmd('Set Appearance 115 "BackG" 0')
+    Cmd('Set Appearance 115 "BackB" 127')
+    Cmd('Set Appearance 102 "BackAlpha" 255')
+
+
+
+
 
 end
 
