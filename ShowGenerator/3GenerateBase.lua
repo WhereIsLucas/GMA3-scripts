8f595ef04c5113f9a1ca53b989ee31487bed5e5c
WhereIsLucas <lucas@retinens.be>
2023-09-03 11:00:12 +0200

    wip

--- ShowGenerator/3GenerateBase.lua
+++ ShowGenerator/3GenerateBase.lua
@@ -32,114 +32,22 @@ function generateGroups()
 end
 
 function generateApparences()
-    Cmd('Store Appearance 101')
-    Cmd('Label Appearance 101 "White"')
-    Cmd('Set Appearance 101 "BackR" 255')
-    Cmd('Set Appearance 101 "BackG" 255')
-    Cmd('Set Appearance 101 "BackB" 255')
-    Cmd('Set Appearance 101 "BackAlpha" 255')
-
-    Cmd('Store Appearance 102')
-    Cmd('Label Appearance 102 "CTO"')
-    Cmd('Set Appearance 102 "BackR" 255')
-    Cmd('Set Appearance 102 "BackG" 210')
-    Cmd('Set Appearance 102 "BackB" 159')
-    Cmd('Set Appearance 102 "BackAlpha" 255')
-
-    Cmd('Store Appearance 103')
-    Cmd('Label Appearance 103 "CTB"')
-    Cmd('Set Appearance 103 "BackR" 159')
-    Cmd('Set Appearance 103 "BackG" 210')
-    Cmd('Set Appearance 103 "BackB" 255')
-    Cmd('Set Appearance 103 "BackAlpha" 255')
-
-    Cmd('Store Appearance 104')
-    Cmd('Label Appearance 104 "Red"')
-    Cmd('Set Appearance 104 "BackR" 255')
-    Cmd('Set Appearance 104 "BackG" 0')
-    Cmd('Set Appearance 104 "BackB" 0')
-    Cmd('Set Appearance 104 "BackAlpha" 255')
-
-    Cmd('Store Appearance 105')
-    Cmd('Label Appearance 105 "Orange"')
-    Cmd('Set Appearance 105 "BackR" 255')
-    Cmd('Set Appearance 105 "BackG" 127')
-    Cmd('Set Appearance 105 "BackB" 0')
-    Cmd('Set Appearance 105 "BackAlpha" 255')
-
-    Cmd('Store Appearance 106')
-    Cmd('Label Appearance 106 "Yellow"')
-    Cmd('Set Appearance 106 "BackR" 255')
-    Cmd('Set Appearance 106 "BackG" 255')
-    Cmd('Set Appearance 106 "BackB" 0')
-    Cmd('Set Appearance 106 "BackAlpha" 255')
-
-    Cmd('Store Appearance 107')
-    Cmd('Label Appearance 107 "Fern Green"')
-    Cmd('Set Appearance 107 "BackR" 127')
-    Cmd('Set Appearance 107 "BackG" 255')
-    Cmd('Set Appearance 107 "BackB" 0')
-
-    Cmd('Store Appearance 108')
-    Cmd('Label Appearance 108 "Green"')
-    Cmd('Set Appearance 108 "BackR" 0')
-    Cmd('Set Appearance 108 "BackG" 255')
-    Cmd('Set Appearance 108 "BackB" 0')
-    Cmd('Set Appearance 108 "BackAlpha" 255')
-
-    Cmd('Store Appearance 109')
-    Cmd('Label Appearance 109 "Sea Green"')
-    Cmd('Set Appearance 109 "BackR" 0')
-    Cmd('Set Appearance 109 "BackG" 255')
-    Cmd('Set Appearance 109 "BackB" 127')
-    Cmd('Set Appearance 109 "BackAlpha" 255')
-
-    Cmd('Store Appearance 110')
-    Cmd('Label Appearance 110 "Cyan"')
-    Cmd('Set Appearance 110 "BackR" 0')
-    Cmd('Set Appearance 110 "BackG" 255')
-    Cmd('Set Appearance 110 "BackB" 255')
-    Cmd('Set Appearance 110 "BackAlpha" 255')
-
-    Cmd('Store Appearance 111')
-    Cmd('Label Appearance 111 "Lavender"')
-    Cmd('Set Appearance 111 "BackR" 127')
-    Cmd('Set Appearance 111 "BackG" 0')
-    Cmd('Set Appearance 111 "BackB" 255')
-    Cmd('Set Appearance 111 "BackAlpha" 255')
-
-    Cmd('Store Appearance 112')
-    Cmd('Label Appearance 112 "Blue"')
-    Cmd('Set Appearance 112 "BackR" 0')
-    Cmd('Set Appearance 112 "BackG" 0')
-    Cmd('Set Appearance 112 "BackB" 255')
-    Cmd('Set Appearance 112 "BackAlpha" 255')
-
-    Cmd('Store Appearance 113')
-    Cmd('Label Appearance 113 "Violet"')
-    Cmd('Set Appearance 113 "BackR" 127')
-    Cmd('Set Appearance 113 "BackG" 0')
-    Cmd('Set Appearance 113 "BackB" 255')
-    Cmd('Set Appearance 113 "BackAlpha" 255')
-
-    Cmd('Store Appearance 114')
-    Cmd('Label Appearance 114 "Magenta"')
-    Cmd('Set Appearance 114 "BackR" 255')
-    Cmd('Set Appearance 114 "BackG" 0')
-    Cmd('Set Appearance 114 "BackB" 255')
-    Cmd('Set Appearance 114 "BackAlpha" 255')
-
-    Cmd('Store Appearance 115')
-    Cmd('Label Appearance 115 "Pink"')
-    Cmd('Set Appearance 115 "BackR" 255')
-    Cmd('Set Appearance 115 "BackG" 0')
-    Cmd('Set Appearance 115 "BackB" 127')
-    Cmd('Set Appearance 115 "BackAlpha" 255')
-
-
-
-
-
+    local colorList = {
+                {
+                    'Name': 'White',
+                    'R': 255,
+                    'G': 255,
+                    'B': 255,
+                    'Gel'
+                },
+            }
+    for i, color in ipairs(colorList) do
+        Cmd('Store Appearance '..100+i..' /Overwrite')
+        Cmd('Label Appearance '..100+i..' "'..color.Name..'"')
+        Cmd('Set Appearance '..100+i..' "BackR" '..color.R)
+        Cmd('Set Appearance '..100+i..' "BackG" '..color.G)
+        Cmd('Set Appearance '..100+i..' "BackB" '..color.B)
+        Cmd('Set Appearance '..100+i..' "BackAlpha" 255')
 end
 
 function main()
